be42b4f27dd3f83143be00cc9011ad8112512c75
CursedWarrior <39302452+CursedWarrior@users.noreply.github.com>
2018-08-13 01:51:50 +0530

    Update NubBot.py

--- NubBot.py
+++ NubBot.py
@@ -271,7 +271,6 @@ async def change_status():
     while True:
         current_status = next(sts)
         await client.change_presence(game = Game(name = "Pokemon Legends"), status = current_status)
-        await asyncio.sleep(1)
 
 
 # List of servers bot is currently in. Updated every 10 mins
