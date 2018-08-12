e4deffd3276d301af901f9aa69125c04ad397919
CursedWarrior <39302452+CursedWarrior@users.noreply.github.com>
2018-08-13 02:34:47 +0530

    Update NubBot.py

--- NubBot.py
+++ NubBot.py
@@ -255,9 +255,9 @@ async def on_message(message):
 
 # Default event during Bot Initiation
 
-@client.event
-async def on_ready():
-    await client.change_presence(game=Game(name="Pokemon Legends"))
+#@client.event
+#async def on_ready():
+#    await client.change_presence(game=Game(name="Pokemon Legends"))
 
 # Magic
 
