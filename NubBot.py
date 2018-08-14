0f85ecd058fa5baf43abd8bec999f738bf9a4c6b
CursedWarrior <39302452+CursedWarrior@users.noreply.github.com>
2018-08-14 21:40:27 +0530

    Update NubBot.py

--- NubBot.py
+++ NubBot.py
@@ -183,9 +183,11 @@ catch_able = ""
 for i in pokemonlist:
     catch_able+=i+"\n"
     
+# catchable Command    
+
 @client.command(brief = "List of catchable pokemon", description = "Check the list of available catchable pokemon in the bot.", pass_context=True)
 async def catchable(ctx):
-    em = Embed(title = "Catchable Pokemon", description = "Here is the list of catchable Pokemon in Nub bot\n\n"+catch_able, colour = Colour())
+    em = Embed(title = "Catchable Pokemon", description = "Here is the list of catchable Pokemon in Nub bot\n\n", colour = Colour.orange())
     await client.send_message(ctx.message.channel, embed = em)
         
     
